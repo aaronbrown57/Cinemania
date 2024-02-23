77d1a93c2b9d33bc96ffe51760f907a74761552d
GrantPrusik <151090707+GrantPrusik@users.noreply.github.com>
2024-02-22 22:20:09 -0500

    Connection to Mongo

--- Cinemania-proj/src/Server.js
+++ Cinemania-proj/src/Server.js
@@ -1,37 +1,31 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-const mongoose = require('mongoose');
-const cors = require('cors');
-const pins = require('./routes/movies');
-app.use('/movies', movies);
-
-// Connect Database
-app.use(cors({ origin: true, credentials: 'http://localhost:3000' }));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello World!'));
+const { MongoClient } = require('mongodb');
 
 
-const conn_str = 'mongodb+srv://grantprusik5:fP0xugxfu5EERhQK@cluster0.m3q4sbt.mongodb.net/';
-mongoose.set('strictQuery', false);
-mongoose.connect(conn_str, {
-    useUnifiedTopology : true,
-    useNewUrlParser : true
-})
-.then(() => {
-    console.log('MongoDB Connection Succeeded...');
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-})
-.catch(err => {
-    console.log('Error in DB Connection ${err}');
-});
+async function main(){
+    
+    const uri = "mongodb+srv://grantprusik5:fP0xugxfu5EERhQK@cluster0.m3q4sbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.get('/home', (req, res) => res.send("hello from home"));
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+
+        
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){ //Function that shows Databases within Mongo Connection (Just for testing)
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
