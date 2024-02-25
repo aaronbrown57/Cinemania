63d7b526af46d60ef55537c120ca89f05e1d67b8
chidubemz <113866146+chidubemz@users.noreply.github.com>
2024-02-25 04:07:58 +0000

    More updates to the login and authenticated view

--- Cinemania-proj/src/Components/Login.js
+++ Cinemania-proj/src/Components/Login.js
@@ -1,33 +1,33 @@
-import { Form } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthView from './AuthView/AuthView';
 const Login = () => {
-    const x = 8;
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Prevent default form submission behavior
+        // Navigate to a different route
+        navigate('/AuthView');
+    };
+
     return (
         <div>
             <h1>This is the log in page</h1>
 
-            <form>
-                <label for="fname">Username:</label>
-                <input type="text" id="Username" name="Username"></input>
-                <br></br>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password"></input>
-                <br></br>
-                <input type="submit"></input>
-                
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="Username">Username:</label>
+                <input type="text" id="Username" name="Username" />
+                <br />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" />
+                <br />
+                <button type="submit">Login</button>
             </form>
+
             <p>Or</p>
-           <Link to="/SignUp">Signup</Link>
-            {/* <Form>  
-                <label for="fname">First name:</label>
-                <input type="text" id="fname" name="fname"></input>
-                <label for="lname">Last name:</label><br></br>
-                <input type="text" id="lname" name="lname"></input>
-            </Form>  */}
+            <Link to="/SignUp">Signup</Link>
         </div>
-
-
-    )
-}
+    );
+};
 
 export default Login;
