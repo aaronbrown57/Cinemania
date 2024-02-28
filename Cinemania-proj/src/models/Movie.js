dbf8c110421fa0ae545924cd8c12b19f36336b76
atech2 <114094405+atech2@users.noreply.github.com>
2024-02-28 14:55:58 -0500

    modified Movie.js schema

--- Cinemania-proj/src/models/Movie.js
+++ Cinemania-proj/src/models/Movie.js
@@ -1,16 +1,64 @@
 const mongoose = require('mongoose');
 
 const MovieSchema = new mongoose.Schema({
-    username: {
+    movieTitle: {
         type: String,
         required: true,
-        trim: true,
     },
-    password: {
+    category: {
         type: String,
         required: true,
-        minLength: 6,
     },
+    cast: {
+        type: Array,
+        required: true,
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    producer: {
+        type: String,
+        required: true,
+    },
+    synopsis: {
+        type: String,
+        required: true,
+    },
+    reviews: {
+        type: Array,
+        required: true,
+    },
+    trailerPictureURL: {
+        type: String,
+        required: true,
+    },
+    trailerVideoURL: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: String,
+        required: true,
+    },
+    comingSoon: {
+        type: Boolean,
+        required: true,
+    },
+    showDate: {
+        type: String,
+        required: function () {
+            //to ask if it is required, if coming soon doesnt need it
+            return this.comingSoon === false;
+        }
+    },
+    showTime: {
+        type: String,
+        required: function () {
+            //to ask if it is required, if coming soon doesnt need it
+            return this.comingSoon === false;
+        }
+    }
 });
 
 module.exports = Movie = mongoose.model('movies', MovieSchema);
