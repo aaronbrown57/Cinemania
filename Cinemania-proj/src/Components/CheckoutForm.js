f0f32df652c361668c3309de53fbb79ab75579c9
aaronbrown57 <95190736+aaronbrown57@users.noreply.github.com>
2024-02-29 13:24:15 -0500

    checkout updated

--- Cinemania-proj/src/Components/CheckoutForm.js
+++ Cinemania-proj/src/Components/CheckoutForm.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {navigate} from 'react-router-dom'
 
+const handleSubmit = () => {
+  // navigate('/order-confirmation', { state: { chosenMovie, showtime, selectedSeats, ticketAges, total } });
+};
 const CheckoutForm = ({ onSubmit }) => {
   return (
     <form onSubmit={onSubmit}>
       {/* Form fields for checkout information */}
-      <button type="submit">Submit</button>
+      <div>
+    <label for="card_number">Credit Card Number:</label>
+    <input type="text" id="card_number" name="card_number" required></input>
+  </div>
+  <div>
+    <label for="expiry_date">Expiry Date:</label>
+    <input type="text" id="expiry_date" name="expiry_date" placeholder="MM/YY" required></input>
+  </div>
+  <div>
+    <label for="cvv">CVV:</label>
+    <input type="text" id="cvv" name="cvv" required></input>
+  </div>
+  <div>
+    <label for="zip_code">Zip Code:</label>
+    <input type="text" id="zip_code" name="zip_code" required></input>
+  </div>
+      <button onClick={handleSubmit} type="submit">Submit</button>
     </form>
   );
 };
