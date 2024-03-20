aaa05bb1d4e2af7400e61006b553efa24697703b
atech2 <114094405+atech2@users.noreply.github.com>
2024-03-20 12:10:57 -0400

    Modifying server to try to query, doesn't work yet

--- Cinemania-proj/src/Server.js
+++ Cinemania-proj/src/Server.js
@@ -1,3 +1,4 @@
+const MovieSchema = require('./models/Movie'); // Import the Movie model from movie.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,7 +9,7 @@ app.use('/movies', users);
 
 // Connect Database
 app.use(cors({ origin: true, credentials: 'http://localhost:3000' }));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -17,17 +18,42 @@ app.use(function(req, res, next) {
 });
 app.use(express.json({ extended: false }));
 
-
 const conn_str = "mongodb+srv://grantprusik5:Temppass2024@cluster0.m3q4sbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.set('strictQuery', false);
 mongoose.connect(conn_str, {
-    useUnifiedTopology : true,
-    useNewUrlParser : true
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 })
-.then(() => {
-    console.log('MongoDB Connection Succeeded...');
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-})
-.catch(err => {
-    console.log('Error in DB Connection ${err}');
-});
+    .then(() => {
+        console.log('MongoDB Connection Succeeded...');
+        app.listen(port, () => console.log(`Server running on port ${port}`));
+
+        const db = mongoose.connection;
+
+        const cursor = db.collection('Movies').find({});
+
+        console.log('Cursor:', cursor);
+
+        const moviesArray = [];
+
+        async function queryMovies() {
+            try {
+
+                try {
+                    cursor.array.forEach(async (doc) => {
+                        moviesArray.push(movie);
+                    });
+                } catch (error) {
+                    console.error('Error processing movie:', error);
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        queryMovies();
+    })
+    .catch(err => {
+        console.log(`Error in DB Connection ${err.message}`);
+    });
