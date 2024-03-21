fd4733910c1db0137f87bf8668adb9cee22003f2
atech2 <114094405+atech2@users.noreply.github.com>
2024-03-21 17:28:02 -0400

    got users

---
+++ Cinemania-proj/src/backend/routes/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+// const jwt = require("jsonwebtoken");
+//const auth = require("../middleware/auth");
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const User = require('../models/User');
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+
+router.use(cors({ origin: true, credentials: 'http://localhost:3000' }));
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+router.post('/newUser', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.json({ msg: 'User added successfully', user: newUser });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// Route to get all users
+router.get('/allUsers', async (req, res) => {
+    try {
+        console.log("Get users was initiated");
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+
+module.exports = router;
