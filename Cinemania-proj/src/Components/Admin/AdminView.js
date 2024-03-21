02260530c6852fe94a4daba779d5c44b825ed6dd
aaronbrown57 <95190736+aaronbrown57@users.noreply.github.com>
2024-03-21 15:49:39 -0400

    took away movies from admin

--- Cinemania-proj/src/Components/Admin/AdminView.js
+++ Cinemania-proj/src/Components/Admin/AdminView.js
@@ -1,12 +1,10 @@
 import React from "react";
 import NavMenu from "../Navigation/NavMenu";
-import MovieDisplay from "../MovieDisplays/MovieDisplay";
 const AdminView = () => {
     return(
         <div className="App">
         <NavMenu loggedIn={true} admin={true}></NavMenu>
         <h1>Welcome Admin_User!</h1>
-        <MovieDisplay isAdmin={true}/>
     </div>
     )
 }
