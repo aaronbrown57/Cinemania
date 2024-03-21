b6382b3e8e861fcc13b14ddc78bd39997246cb2d
atech2 <114094405+atech2@users.noreply.github.com>
2024-03-21 18:25:28 -0400

    changed server port

--- Cinemania-proj/src/backend/routes/movies.js
+++ Cinemania-proj/src/backend/routes/movies.js
@@ -10,9 +10,9 @@ const Movie = require('../models/Movie');
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 
-router.use(cors({ origin: true, credentials: 'http://localhost:3000' }));
+router.use(cors({ origin: true, credentials: 'http://localhost:5000' }));
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    res.header("Access-Control-Allow-Origin", 'http://localhost:5000');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
