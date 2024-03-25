32599bc53b6707eed580165a86a5812eb33d7a38
aaronbrown57 <95190736+aaronbrown57@users.noreply.github.com>
2024-03-24 21:12:24 -0400

    fixed movielist

--- Cinemania-proj/src/Components/MovieDisplays/MovieList.js
+++ Cinemania-proj/src/Components/MovieDisplays/MovieList.js
@@ -1,37 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Movie from './Movie'; // Assuming you have a Movie component
 
 const MovieList = (props) => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get('http://127.0.0.1:5000/movies/allMovies');
-        setMovies(response.data); // Assuming your API returns an array of movies
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      }
-    };
-
-    fetchMovies();
-  }, []); // Empty dependency array to only run the effect once when the component mounts
-
-  return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <Movie
-          isAdmin={props.isAdmin}
-          key={movie.id}
-          title={movie.title}
+    return (
+      <div className="movie-list">
+     
+        {props.items.map((movie) => (
+          <Movie isAdmin={props.isAdmin} showing={props.showing}
+          key={movie._id}
+          title={movie.movieTitle}
           pieces={movie.director}
-          img={movie.img}
-          trailer={movie.trailer}
-        />
-      ))}
-    </div>
-  );
-};
+          img={movie.trailerPictureURL}
+          trailer={movie.trailerVideoURL}
+          />
+        ))} 
+     
+      </div>
+    );
+  }
 
-export default MovieList;
+  export default MovieList;
