f487596fb97fbafa7c845b0b097830d1cc0d9c88
chidubemz <113866146+chidubemz@users.noreply.github.com>
2024-03-25 20:30:13 +0000

    now works with authentication and takes you to auth view

--- Cinemania-proj/backend/routes/users.js
+++ Cinemania-proj/backend/routes/users.js
@@ -72,17 +72,47 @@ router.post("/signup", async (req, res) => {
 });
 
 
-router.post('/login', async (req, res) => {
+//Login Route
+router.post("/login", async (req,res) => {
     try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email, password });
-        if (user) {
-            res.json({ success: true, message: 'Login successful', user });
-        } else {
-            res.status(401).json({ success: false, message: 'Invalid email or password' });
+        const {email, password} = req.body;
+        if (!email || !password) {
+            return res.status(400).json({msg: "Please enter all the fields!"});
         }
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Failed to login', error: error.message });
+
+        const user = await User.findOne({ email});
+        if (!user) {
+            return res
+                .status(400)
+                .send({msg: "User with this email does not exists"});
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).send({msg: "Incorrect password."});
+        }
+        const token = jwt.sign({ id: user._id}, "passwordKey");
+        res.json({ token, user: { id: user._id, username: user.username } });
+        console.log('Received login request');
+    } catch (err) {
+        res.status(500).json({ error: err.message});
+    }
+});
+
+//CHECK TOKEN VALID
+router.post("/tokenIsValid", async(req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.json(false);
+        const verified = jwt.verify(token, "passwordKey");
+        if (!verified) return res.json(false);
+        const user = await User.findById(verified.id);
+        if (!user) return res.json(false);
+        console.log('Received token request');
+        return res.json(true);
+    } catch (err) {
+        res.status(500).json({ error: err.message});
     }
 });
 
