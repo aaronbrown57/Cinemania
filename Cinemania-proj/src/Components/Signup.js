0f637075efcc5945689501917a31a8d90b30d979
chidubemz <113866146+chidubemz@users.noreply.github.com>
2024-03-25 20:03:59 +0000

    added some endpoints and got user signups to work 401 error

--- Cinemania-proj/src/Components/Signup.js
+++ Cinemania-proj/src/Components/Signup.js
@@ -1,69 +1,79 @@
-import { useState } from 'react'; // Import useState to use state variables
+import { useState, useContext } from 'react'; // Import useState to use state variables
 import { Link, useNavigate } from "react-router-dom";
 import NavMenu from "./Navigation/NavMenu";
 import ConfirmAccountCreation from "./ConfirmAccountCreation";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import CreditCardInput from "react-credit-card-input";
-import CreditCardIntake from "./CreditCardIntake";
-
-
+import { UserContext } from '../context/UserContext';
+import axios from 'axios';
 function SignUp() {
+  const [datastate, setData] = useState({
+   
+   firstName: "",
+    lastName: "",
+    email:"",
+    phone:"",
+    password:"",
+    confirmPassword:"",
+    creditCard:{
+      cardNumber: "",
+      expiry: "",
+    },
+    billingAddress:"",
+    homeAddress: "",
+    promoSubscription:false,
+});
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const [error, setError] = useState(''); // Define error state variable with initial value ''
+  const { setUserData } = useContext(UserContext);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    
-    // Check if the event target is an HTMLFormElement
-    if (e.target instanceof HTMLFormElement) {
-      const formData = new FormData(e.target); // Access form data directly from the event target
-      const requiredFields = [
-        "firstname",
-        "lastname",
-        "email",
-        "password",
-        "confirmPassword",
-        "billingAddress",
-        "homeAddress",
-      ];
-  
-      const allFieldsFilled = requiredFields.every((field) => formData.get(field));
-  
-      if (!allFieldsFilled) {
-        setError("Please fill out all required fields.");
-        return;
+
+
+  // Handler for updating credit card information
+  const handleCreditCardChange = ({ target }) => {
+    setData({
+      ...datastate,
+      creditCard: {
+        ...datastate.creditCard,
+        [target.name]: target.value
       }
-  
-      // Convert formData to JSON object
-      const userData = {};
-      formData.forEach((value, key) => {
-        userData[key] = value;
-      });
-  
-      try {
-        const response = await fetch('./../backend/routes/users/newUser', {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(userData),
-        });
-  
-        if (!response.ok) {
-          throw new Error("Failed to create account");
-        }
-  
-        console.log("Account created successfully!");
-        navigate("/ConfirmAccountCreation");
-      } catch (error) {
-        setError("Error creating account: " + error.message);
-      }
-    } else {
-      setError("Invalid form submission.");
-    }
+    });
   };
 
+  const handleSubmit= async (e)=> {
+      e.preventDefault();
+      setLoading(true);
+      try {
+        const newUser = {...datastate};
+        
+          // Check if correct axios stuff
+       
+        console.log("New User Data:", newUser);
+          await axios.post("http://localhost:5000/users/signup", newUser);
+
+          const loginRes = await axios.post("http://localhost:5000/users/login", {
+            email: newUser.email, // needed for login
+            password: newUser.password
+          });
+
+          console.log("Login Response:", loginRes);
+
+
+      setUserData({
+          token:loginRes.data.token,
+          user:loginRes.data.user,
+      });
+      localStorage.setItem("auth-token",loginRes.data.token);
+      setLoading(false);
+      navigate('/authView')
+        }catch (err) {
+          console.error("Error:", err);
+          setLoading(false);
+          err.response.data.msg && setError(err.response.data.msg);
+      }
+  }
   return (
     <div className="signupDiv">
       <NavMenu></NavMenu>
@@ -71,54 +81,139 @@ function SignUp() {
       <Form className="login-form" onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicFirstName">
           <Form.Label>First name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your first name" name="firstname" />
+          <Form.Control 
+          type="text"
+           placeholder="Enter your first name" 
+           name="firstName"
+           value={datastate.firstName} 
+           onChange={(e) => setData({ ...datastate, firstName: e.target.value })} 
+           required
+           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicLastName">
           <Form.Label>Last name</Form.Label>
-          <Form.Control type="lastname" placeholder="Enter your last name" name="lastname"/>
+          <Form.Control 
+          type="text" 
+          placeholder="Enter your last name" 
+          name="lastName"
+          value={datastate.lastName} 
+           onChange={(e) => setData({ ...datastate, lastName: e.target.value })} 
+           required
+           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email Address</Form.Label>
-          <Form.Control type="firstname" placeholder="Enter your email" name="email"/>
+          <Form.Control 
+          type="email"
+          placeholder="Enter your email" 
+          name="email"
+          value={datastate.email} 
+           onChange={(e) => setData({ ...datastate, email: e.target.value })} 
+           required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="tel" placeholder="Enter phone number" name="phoneNumber"/>
+          <Form.Control 
+          type="tel" 
+          placeholder="Enter phone number" 
+          name="phoneNumber"
+          value={datastate.phone} 
+           onChange={(e) => setData({ ...datastate, phone: e.target.value })} 
+           required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter a password" name="password"/>
+          <Form.Control 
+          type="password" 
+          placeholder="Enter a password" 
+          name="password"
+          value={datastate.password} 
+          onChange={(e) => setData({ ...datastate, password: e.target.value })} 
+          required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Confirm password" name="confirmPassword"/>
+          <Form.Control 
+          type="password"
+           placeholder="Confirm password" 
+           name="confirmPassword"
+           value={datastate.confirmPassword} 
+           onChange={(e) => setData({ ...datastate, confirmPassword: e.target.value })} 
+           required
+           />
         </Form.Group>
 
         <br></br>
 
-        <CreditCardInput />
+        <CreditCardInput
+  cardNumberInputProps={{
+    value: datastate.creditCard.cardNumber,
+    onChange: (e) => {
+      setData({
+        ...datastate,
+        creditCard: {
+          ...datastate.creditCard,
+          cardNumber: e.target.value
+        }
+      });
+    }
+  }}
+  cardExpiryInputProps={{
+    value: datastate.creditCard.expiry, 
+    onChange: (e) => {
+      setData({
+        ...datastate,
+        creditCard: {
+          ...datastate.creditCard,
+          expiry: e.target.value
+        }
+      });
+    }
+  }}
+  fieldClassName="input"
+/>
+
 
         <Form.Group className="mb-3" controlId="formBasicBillingAddress">
           <Form.Label>Billing Address</Form.Label>
-          <Form.Control type="address" placeholder="Enter your Billing Address" name="billingAddress"/>
+          <Form.Control 
+          type="address" 
+          placeholder="Enter your Billing Address" 
+          name="billingAddress"
+          value={datastate.billingAddress} 
+        onChange={(e) => setData({ ...datastate, billingAddress: e.target.value })} 
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicHomeAddress">
           <Form.Label>Home Address</Form.Label>
-          <Form.Control type="address" placeholder="Enter your Home Address" name="homeAddress"/>
+          <Form.Control 
+          type="address"
+           placeholder="Enter your Home Address" 
+           name="homeAddress"
+           value={datastate.homeAddress} 
+        onChange={(e) => setData({ ...datastate, homeAddress: e.target.value })} 
+        required
+           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check
-            type="checkbox"
-            label="Subscribe to Promotional Content"
-          />
-        </Form.Group>
+  <Form.Check
+    type="checkbox"
+    label="Subscribe to Promotional Content"
+    checked={datastate.promoSubscription} // Bind checked prop to promoSubscription state
+    onChange={(e) => setData({ ...datastate, promoSubscription: e.target.checked })} // Handle checkbox change
+  />
+</Form.Group>
+
 
         <Button variant="primary" type="submit" >
           Register
