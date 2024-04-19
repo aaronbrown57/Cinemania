0c1d363cc54a1cc1bf82a4b192297ea7e4f61073
atech2 <114094405+atech2@users.noreply.github.com>
2024-04-19 14:34:30 -0400

    promotions and sending to users that are subscribed

--- Cinemania-proj/backend/routes/promotion.js
+++ Cinemania-proj/backend/routes/promotion.js
@@ -1,6 +1,67 @@
 const express = require('express');
 const router = express.Router();
 const Room = require('../models/Promotion');
+const nodemailer = require('nodemailer');
+
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 465,
+    secure: true,
+    secureConnection:false,
+    debug:true,
+    logger:true,
+        auth: {
+            user: 'cinemaniateam@gmail.com',
+            pass: 'balx bias kmmh btku'
+        },
+    tls:{
+        rejectUnauthorized:true
+    }
+});
+
+router.put('/sendPromo/:promotionId', async (req, res) => {
+    const { promotionId } = req.params;
+    const { email } = req.body;
+
+    console.log('Received request to send promo:', promotionId, email); // Debugging statement
+  
+    try {
+      // Send the promo email
+      const mailOptions = {
+        from: 'cinemaniateam@gmail.com',
+        to: email,
+        subject: 'New Promotion Available!',
+        text: `Thank you for signing up for email promotions. A new promotion code is available to you! ${promotionId}`,
+      };
+
+      console.log('Sending promo email to:', email); // Debugging statement
+      await transporter.sendMail(mailOptions);
+
+      console.log('Promo email sent successfully to:', email); // Debugging statement
+  
+      // Update sentToUsers status
+      // Assuming you have a MongoDB or similar database
+      const updatedPromotion = await Promotion.findOneAndUpdate(
+        { _id: promotionId },
+        { sentToUsers: true },
+        { new: true }
+      );
+
+      if (!updatedPromotion) {
+        console.error('Promotion not found:', promotionId); // Debugging statement
+        return res.status(404).json({ error: 'Promotion not found.' });
+      }
+
+      console.log('Promotion sent to users successfully:', updatedPromotion); // Debugging statement
+
+      res.status(200).json({ message: 'Promotion sent to users successfully.', updatedPromotion });
+    } catch (error) {
+      console.error('Error sending promotion to users:', error.message); // Debugging statement
+      res.status(500).json({ error: 'Failed to send promotion to users.' });
+    }
+  });
+
 
 
 router.post('/addPromotion', async (req, res) => {
--- Cinemania-proj/src/Components/ManagePromos.js
+++ Cinemania-proj/src/Components/ManagePromos.js
@@ -4,6 +4,15 @@ import "./css/ManagePromotions.css";
 
 const ManagePromotions = () => {
   const [promotions, setPromotions] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState({
+    promoCode: "",
+    start: "",
+    end: "",
+    percentage: "",
+    description: "",
+  });
+  const [updateMessage, setUpdateMessage] = useState(""); // State for update message
 
   useEffect(() => {
     const fetchPromotions = async () => {
@@ -33,10 +42,9 @@ const ManagePromotions = () => {
   const handleCheckboxChange = async (promotionId, isActive, sentToUsers) => {
     console.log("Checkbox changed:", promotionId, isActive, sentToUsers); // Debugging statement
 
-    if (sentToUsers && !isActive) {
-      // Display error toast
-      console.error("Already sent to users, cannot disable at this time");
-      // Optionally, you can show a toast notification or error message here
+    if (sentToUsers) {
+      // Display error message
+      setUpdateMessage("Already sent to users, cannot modify");
       return;
     }
 
@@ -60,6 +68,8 @@ const ManagePromotions = () => {
           promo._id === updatedPromotion._id ? updatedPromotion : promo
         )
       );
+      // Set update message
+      setUpdateMessage("Promotion updated, please refresh page.");
       // Optionally, you can show a success message or notification here
       console.log("Promotion updated successfully!");
     } catch (error) {
@@ -68,10 +78,130 @@ const ManagePromotions = () => {
     }
   };
 
+  const handleSendToUsers = async (promotionId) => {
+    try {
+      // Fetch all users with promoSubscription turned on
+      const usersResponse = await fetch("http://localhost:5000/users/allUsers");
+      if (!usersResponse.ok) {
+        throw new Error("Failed to fetch users");
+      }
+      const usersData = await usersResponse.json();
+      const subscribedUsers = usersData.filter(
+        (user) => user.promoSubscription
+      );
+
+      if (subscribedUsers.length === 0) {
+        console.error("No subscribed users found");
+        setUpdateMessage("No subscribed users found.");
+        return;
+      }
+
+      // Prepare promo email for each subscribed user and send
+      const sendPromoEmails = subscribedUsers.map(async (user) => {
+        const mailOptions = {
+          from: "cinemaniateam@gmail.com",
+          to: user.email,
+          subject: "New Promotion Available!",
+          text: `Thank you for signing up for email promotions. A new promotion code is available to you! ${promotionId}`,
+        };
+
+        const responseEmail = await fetch(
+          `http://localhost:5000/promotion/sendPromo/${promotionId}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: user.email }), // Pass the email parameter
+          }
+        );
+
+        if (!responseEmail.ok) {
+          throw new Error(`Failed to send promotion email to ${user.email}`);
+        }
+
+        return responseEmail.json();
+      });
+
+      // Wait for all promo emails to be sent
+      const sentPromoEmails = await Promise.all(sendPromoEmails);
+      console.log("Promotion emails sent:", sentPromoEmails);
+
+      // Update the promotion's sentToUsers status
+      const response = await fetch(
+        `http://localhost:5000/promotion/sendToUsers/${promotionId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ sentToUsers: true }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to send promotion to users");
+      }
+      const updatedPromotion = await response.json();
+      setPromotions((prevPromotions) =>
+        prevPromotions.map((promo) =>
+          promo._id === updatedPromotion._id ? updatedPromotion : promo
+        )
+      );
+      // Set update message
+      setUpdateMessage("Promotion sent to subscribed users successfully.");
+      // Optionally, you can show a success message or notification here
+      console.log("Promotion sent to subscribed users successfully!");
+    } catch (error) {
+      console.error("Error sending promotion to users:", error.message);
+      // Optionally, you can show an error message or notification here
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "http://localhost:5000/promotion/addPromotion",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to add promotion");
+      }
+      const newPromotion = await response.json();
+      setPromotions([...promotions, newPromotion]);
+      setShowForm(false);
+      setFormData({
+        promoCode: "",
+        start: "",
+        end: "",
+        percentage: "",
+        description: "",
+      });
+      console.log("Promotion added successfully!");
+    } catch (error) {
+      console.error("Error adding promotion:", error.message);
+    }
+  };
+
   return (
     <div>
       <NavMenu loggedIn={true} admin={true}></NavMenu>
       <h2>Manage Promotions</h2>
+      <div className="update-message">{updateMessage}</div>{" "}
+      {/* Update message */}
+      <div className="checkbox-label">Is Active</div>{" "}
+      {/* Label above checkboxes */}
       {promotions.map((promotion) => (
         <div className="promotion-item" key={promotion._id}>
           <label className="checkbox-label">
@@ -85,20 +215,71 @@ const ManagePromotions = () => {
                   promotion.sentToUsers
                 )
               }
-              onClick={() =>
-                console.log(
-                  "Checkbox clicked:",
-                  promotion._id,
-                  promotion.isActive,
-                  promotion.sentToUsers
-                )
-              } // Debugging statement for click event
             />
             <span className="promotion-title">{promotion.title}</span> Promo
             Code: {promotion.promoCode} - Description: {promotion.description}
           </label>
+          {!promotion.sentToUsers && ( // Render button if promotion has not been sent to users
+            <button onClick={() => handleSendToUsers(promotion._id)}>
+              Send to Users
+            </button>
+          )}
         </div>
       ))}
+      <button onClick={() => setShowForm(true)}>Add Promotion</button>
+      {showForm && (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Promo Code:
+            <input
+              type="text"
+              name="promoCode"
+              value={formData.promoCode}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <label>
+            Start Date:
+            <input
+              type="text"
+              name="start"
+              value={formData.start}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <label>
+            End Date:
+            <input
+              type="text"
+              name="end"
+              value={formData.end}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <label>
+            Percentage:
+            <input
+              type="number"
+              name="percentage"
+              value={formData.percentage}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <label>
+            Description:
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleInputChange}
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 };
