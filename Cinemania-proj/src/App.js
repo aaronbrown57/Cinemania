9cdcaa12a5e08f58f9083a9c410ef47122f788c5
chidubemz <113866146+chidubemz@users.noreply.github.com>
2024-05-01 08:33:57 +0000

    filled out remove movies

--- Cinemania-proj/src/App.js
+++ Cinemania-proj/src/App.js
@@ -27,6 +27,7 @@ import ManagePromos from "./Components/ManagePromos"
 import ManageMovies from "./Components/ManageMovies.js"
 import { UserContext } from './context/UserContext.js';
 import Edit from "./Components/Edit.js";
+import RemoveMovie from './Components/RemoveMovie.js';
 
 
 const App = () => {
@@ -100,11 +101,12 @@ const App = () => {
           <Route path='/checkout' element={<CheckoutForm />} />
           <Route path='/order-confirmation' element={<OrderConfirmation />} />
           {/* Routes for Admin processes */}
-          <Route path='/manage-movies' element={<ManageMovies />} />
+          <Route path='/manage-Movies' element={<ManageMovies />} />
           <Route path='/manage-promos' element={<ManagePromos />} />
           <Route path='/add-showtime' element ={<AddShowtime />} />
           <Route path='/remove-showtime' element ={<RemoveShowtime />} />
           <Route path='/search' element={<SearchResultsPage />} />
+          <Route path='/remove-movie' element={<RemoveMovie/>}/>
         </Routes>
     
       </Router>
