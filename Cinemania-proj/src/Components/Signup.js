492d874e7c5fe38341a1377be4b2c54d78be4492
atech2 <114094405+atech2@users.noreply.github.com>
2024-04-30 22:24:48 -0400

    signup credit card add

--- Cinemania-proj/src/Components/Signup.js
+++ Cinemania-proj/src/Components/Signup.js
@@ -1,15 +1,14 @@
-import { useState, useContext } from 'react'; // Import useState to use state variables
+import { useState, useContext } from "react"; // Import useState to use state variables
 import { Link, useNavigate } from "react-router-dom";
 import NavMenu from "./Navigation/NavMenu";
 import ConfirmAccountCreation from "./ConfirmAccountCreation";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import CreditCardInput from "react-credit-card-input";
-import { UserContext } from '../context/UserContext';
-import axios from 'axios';
+import { UserContext } from "../context/UserContext";
+import axios from "axios";
 function SignUp() {
   const [datastate, setData] = useState({
-
     firstName: "",
     lastName: "",
     email: "",
@@ -23,7 +22,7 @@ function SignUp() {
     billingAddress: "",
     homeAddress: "",
     promoSubscription: false,
-    type: 1
+    type: 1,
   });
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -31,7 +30,10 @@ function SignUp() {
   const [registrationCompleted, setRegistrationCompleted] = useState(false);
   const { setUserData } = useContext(UserContext);
 
-
+  const [creditCard, setCreditCard] = useState({
+    cardNumber: "",
+    expiry: "",
+  });
 
   // Handler for updating credit card information
   const handleCreditCardChange = ({ target }) => {
@@ -39,47 +41,46 @@ function SignUp() {
       ...datastate,
       creditCard: {
         ...datastate.creditCard,
-        [target.name]: target.value
-      }
+        [target.name]: target.value,
+      },
     });
   };
-
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-
+  
     try {
-      const newUser = { ...datastate };
-
+      const newUser = { ...datastate, creditCard }; // Include credit card information in the user data
+  
       // Check if correct axios stuff
-
+  
       console.log("New User Data:", newUser);
       await axios.post("http://localhost:5000/users/signup", newUser);
-
+  
       const loginRes = await axios.post("http://localhost:5000/users/login", {
         email: newUser.email, // needed for login
         password: newUser.password
       });
-
+  
       console.log("Login Response:", loginRes);
-
-
+  
       setUserData({
         token: loginRes.data.token,
         user: loginRes.data.user,
       });
       localStorage.setItem("auth-token", loginRes.data.token);
       setLoading(false);
-      
+  
     } catch (err) {
       console.error("Error:", err);
       setLoading(false);
       err.response.data.msg && setError(err.response.data.msg);
     }
     setRegistrationCompleted(true);
-  }
+  };
+  
+
   return (
     <div className="signupDiv">
       <NavMenu></NavMenu>
@@ -92,7 +93,9 @@ function SignUp() {
             placeholder="Enter your first name"
             name="firstName"
             value={datastate.firstName}
-            onChange={(e) => setData({ ...datastate, firstName: e.target.value })}
+            onChange={(e) =>
+              setData({ ...datastate, firstName: e.target.value })
+            }
             required
           />
         </Form.Group>
@@ -104,7 +107,9 @@ function SignUp() {
             placeholder="Enter your last name"
             name="lastName"
             value={datastate.lastName}
-            onChange={(e) => setData({ ...datastate, lastName: e.target.value })}
+            onChange={(e) =>
+              setData({ ...datastate, lastName: e.target.value })
+            }
             required
           />
         </Form.Group>
@@ -140,7 +145,9 @@ function SignUp() {
             placeholder="Enter a password"
             name="password"
             value={datastate.password}
-            onChange={(e) => setData({ ...datastate, password: e.target.value })}
+            onChange={(e) =>
+              setData({ ...datastate, password: e.target.value })
+            }
             required
           />
         </Form.Group>
@@ -152,7 +159,9 @@ function SignUp() {
             placeholder="Confirm password"
             name="confirmPassword"
             value={datastate.confirmPassword}
-            onChange={(e) => setData({ ...datastate, confirmPassword: e.target.value })}
+            onChange={(e) =>
+              setData({ ...datastate, confirmPassword: e.target.value })
+            }
             required
           />
         </Form.Group>
@@ -161,32 +170,17 @@ function SignUp() {
 
         <CreditCardInput
           cardNumberInputProps={{
-            value: datastate.creditCard.cardNumber,
-            onChange: (e) => {
-              setData({
-                ...datastate,
-                creditCard: {
-                  ...datastate.creditCard,
-                  cardNumber: e.target.value
-                }
-              });
-            }
+            value: creditCard.cardNumber,
+            onChange: (e) =>
+              setCreditCard({ ...creditCard, cardNumber: e.target.value }),
           }}
           cardExpiryInputProps={{
-            value: datastate.creditCard.expiry,
-            onChange: (e) => {
-              setData({
-                ...datastate,
-                creditCard: {
-                  ...datastate.creditCard,
-                  expiry: e.target.value
-                }
-              });
-            }
+            value: creditCard.expiry,
+            onChange: (e) =>
+              setCreditCard({ ...creditCard, expiry: e.target.value }),
           }}
           fieldClassName="input"
         />
-
 
         <Form.Group className="mb-3" controlId="formBasicBillingAddress">
           <Form.Label>Billing Address</Form.Label>
@@ -195,7 +189,9 @@ function SignUp() {
             placeholder="Enter your Billing Address"
             name="billingAddress"
             value={datastate.billingAddress}
-            onChange={(e) => setData({ ...datastate, billingAddress: e.target.value })}
+            onChange={(e) =>
+              setData({ ...datastate, billingAddress: e.target.value })
+            }
           />
         </Form.Group>
 
@@ -206,7 +202,9 @@ function SignUp() {
             placeholder="Enter your Home Address"
             name="homeAddress"
             value={datastate.homeAddress}
-            onChange={(e) => setData({ ...datastate, homeAddress: e.target.value })}
+            onChange={(e) =>
+              setData({ ...datastate, homeAddress: e.target.value })
+            }
             required
           />
         </Form.Group>
@@ -216,7 +214,9 @@ function SignUp() {
             type="checkbox"
             label="Subscribe to Promotional Content"
             checked={datastate.promoSubscription} // Bind checked prop to promoSubscription state
-            onChange={(e) => setData({ ...datastate, promoSubscription: e.target.checked })} // Handle checkbox change
+            onChange={(e) =>
+              setData({ ...datastate, promoSubscription: e.target.checked })
+            } // Handle checkbox change
           />
         </Form.Group>
 
@@ -232,14 +232,17 @@ function SignUp() {
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit" >
+        <Button variant="primary" type="submit">
           Register
         </Button>
       </Form>
-      {registrationCompleted && <ConfirmAccountCreation
-        userEmail={datastate.email}
-        userName={datastate.firstName}
-        userType={datastate.type} />}
+      {registrationCompleted && (
+        <ConfirmAccountCreation
+          userEmail={datastate.email}
+          userName={datastate.firstName}
+          userType={datastate.type}
+        />
+      )}
     </div>
   );
 }
